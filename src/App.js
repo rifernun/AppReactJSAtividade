2038d2712ef0fea6bf75ed75f0708ca9148453fe
230222 <230222@LTI4105>
2025-05-22 15:18:31 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          quer saber mais sobre?
+        </h1>
       </header>
     </div>
   );
